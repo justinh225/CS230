b6eaaa7cd960ee57889b21a237e1cfd3a750048c
justinh225 <jah00031@mix.wvu.edu>
2023-04-10 12:14:32 -0400

    Revert "Class notes"
    This reverts commit afe78de95d325ee3fefdcbdb9a14247435b0f7db.

--- src/app/TopNavigation/profile-bar/profile-data.component.ts
+++ src/app/TopNavigation/profile-bar/profile-data.component.ts
@@ -8,9 +8,6 @@ import { ProfileDataModel } from "./profile-data.model";
     templateUrl: 'profile-data.component.html'
 })
 
-/**
- * Includes Class Notes for implementing with an array
- */
 export class ProfileData implements OnInit {
     profileData: ProfileDataModel | undefined;
 
@@ -25,15 +22,11 @@ export class ProfileData implements OnInit {
     }
 
     getProfileData() {
-        //to get an array, we can specify an array of ProfileDataModels
-        //return this.http.get<ProfileDataModel []>('https://cs230-f05ab-default-rtdb.firebaseio.com/profile.json');
         return this.http.get<ProfileDataModel>('https://cs230-f05ab-default-rtdb.firebaseio.com/profile.json');
     }
     showProfileData() {
-        //subscript to an array instead of ProfileDataModel
         this.getProfileData().subscribe((data: ProfileDataModel) => {
             console.log(data);
-            //dont set profile data equal to the profile data, push the data to the array
             this.profileData = data;
         })
     }
